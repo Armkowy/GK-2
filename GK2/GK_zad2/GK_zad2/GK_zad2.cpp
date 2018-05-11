e7b00dcf553257e20c3eea2cac53409f76da0328
Łukasz <armkowy@gmail.com>
2018-05-11 10:50:04 +0200

    Upload 2

---
+++ GK2/GK_zad2/GK_zad2/GK_zad2.cpp
@@ -0,0 +1,221 @@
+﻿// lab2_zad2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "math.h"
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define GL_PI 3.1415f
+
+enum
+{
+	EXIT                            // wyjęcie
+};
+
+// Wielkoć obrotów
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+
+static GLboolean iCull, iDepth, iOutline;
+
+// Funkcja wykonuje wszystkie konieczne inicjalizacje kontekstu renderowania
+void SetupRC()
+{
+	// Czarne tło
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Kolor rysujšcy - zielony
+	glColor3f(0.25f, 0.42f, 1.0f);
+	// Model cieniowania kolorów - płaski
+	glShadeModel(GL_FLAT);
+	// Wielokšty o nawinięciu zgodnym z ruchem wskazówek zegara traktowane sš
+	// jako skierowane do przodu. Takie ustawienie jest konieczne, ponieważ
+	// korzystamy z wachlarzy trójkštów.
+	glFrontFace(GL_CW);
+}
+// Wywoływana w celu przerysowania sceny
+void RenderScene(void)
+{
+	GLfloat x, y, angle; // Przechowujš wartoci współrzędnych i kšta
+	int iPivot = 1; // Do oznaczania zamiennych kolorów
+					// Wyczyszczenie okna i bufora głębi
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Włšczenie lub wyłšczenie mechanizmu eliminowania ukrytych powierzchni
+	if (iCull)
+		glEnable(GL_CULL_FACE);
+	else
+		glDisable(GL_CULL_FACE);
+	// Włšczenie lub wyłšczenie mechanizmu sprawdzania głębi
+	if (iDepth)
+		glEnable(GL_DEPTH_TEST);
+	else
+		glDisable(GL_DEPTH_TEST);
+	// Jeżeli ten znacznik będzie ustawiony, to wielokšty zwrócone
+	// tyłem do widza będš rysowane tylko w postaci szkieletu
+	if (iOutline)
+		glPolygonMode(GL_BACK, GL_LINE);
+	else
+		glPolygonMode(GL_BACK, GL_FILL);
+	// Zapisanie stanu macierzy i wykonanie obrotu
+	glPushMatrix();
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+	// Rozpoczęcie rysowania wachlarza trójkštów
+	glBegin(GL_TRIANGLE_FAN);
+	// Czubek stoka jest wspólnym wierzchołkiem wszystkich trójkštów z wachlarza
+	// wysuniętym do góry w osi z. W ten sposób zamiast koła powstanie stożek.
+	glVertex3f(0.0f, 0.0f, 75.0f);
+	// Wykonujemy obrót w około i oznaczamy w równych odstępach wierzchołki
+	// tworzšce wachlarz trójkštów.
+	for (angle = 0.0f; angle <= (2.5f*GL_PI); angle += (GL_PI / 9.5f))
+	{
+		// Wyliczenie współrzędnych x i y kolejnego wierzchołka
+		x = 50.0f*sin(angle);
+		y = 50.0f*cos(angle);
+		// Wybieranie koloru - zielony lub czerwony
+		if ((iPivot % 2) == 0)
+			glColor3f(0.25f, 0.42f, 1.0f);
+		else
+			glColor3f(0.25f, 0.0f, 0.0f);
+		// Inkrementacja zmiennej okrelajšcej rodzaj koloru
+		iPivot++;
+		// Definiowanie kolejnego wierzchołka w wachlarzu trójkštów
+		glVertex2f(x, y);
+	}
+	// Zakoczenie rysowania trójkštów stożka
+	glEnd();
+	// Rozpoczęcie rysowania kolejnego wachlarza trójkštów
+	// zakrywajšcego podstaw stoka
+	glBegin(GL_TRIANGLE_FAN);
+	// rodek wachlarza znajduje się na poczštku układu współrzędnych
+	glVertex2f(0.0f, 0.0f);
+	for (angle = 0.0f; angle <= (2.5f*GL_PI); angle += (GL_PI / 9.5f))
+	{
+		// Wyliczenie współrzędnych x i y kolejnego wierzchołka
+		x = 50.0f*sin(angle);
+		y = 50.0f*cos(angle);
+		// Wybieranie koloru - zielony lub czerwony
+		if ((iPivot % 2) == 0)
+			glColor3f(0.25f, 0.42f, 1.0f);
+		else
+			glColor3f(0.25f, 0.0f, 0.0f);
+		// Inkrementacja zmiennej okrelajšcej rodzaj koloru
+		iPivot++;
+		// Definiowanie kolejnego wierzchołka w wachlarzu trójkštów
+		glVertex2f(x, y);
+	}
+	// Zakoczenie rysowania trójkštów podstawy stoka
+	glEnd();
+	// Odtworzenie macierzy przekształceń
+	glPopMatrix();
+	// Wysłanie poleceń do wykonania
+	glutSwapBuffers();
+}
+
+void SpecialKeys(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+		xRot -= 5.0f;
+	if (key == GLUT_KEY_DOWN)
+		xRot += 5.0f;
+	if (key == GLUT_KEY_LEFT)
+		yRot -= 5.0f;
+	if (key == GLUT_KEY_RIGHT)
+		yRot += 5.0f;
+	if (key > 356.0f)
+		xRot = 0.0f;
+	if (key < -1.0f)
+		xRot = 355.0f;
+	if (key > 356.0f)
+		yRot = 0.0f;
+	if (key < -1.0f)
+		yRot = 355.0f;
+	// Odwieżenie zawartoci okna
+	glutPostRedisplay();
+}
+void ChangeSize(int w, int h)
+{
+	GLfloat nRange = 100.0f;
+	// Zabezpieczenie przed dzieleniem przez zero
+	if (h == 0)
+		h = 1;
+	// Ustalenie wymiarów widoku na zgodnych z wymiarami okna
+	glViewport(0, 0, w, h);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Utworzenie przestrzeni ograniczajšcej (lewo, prawo, dół, góra, blisko, daleko)
+	if (w <= h)
+		glOrtho(-nRange, nRange, -nRange * h / w, nRange*h / w, -nRange, nRange);
+	else
+		glOrtho(-nRange * w / h, nRange*w / h, -nRange, nRange, -nRange, nRange);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void Menu(int value)
+{
+	switch (value)
+	{
+		//
+	case GL_CULL_FACE:
+		iCull = !iCull;
+		glutSetWindowTitle("Stozek - GL_CULL_FACE");
+		RenderScene();
+		break;
+
+		//
+	case GL_DEPTH_TEST:
+		iDepth = !iDepth;
+		glutSetWindowTitle("Stozek - GL_DEPTH_TEST");
+		RenderScene();
+		break;
+
+		//
+	case GL_LINE:
+		iOutline = true;
+		glutSetWindowTitle("Stozek - GL_LINE");
+		RenderScene();
+		break;
+
+		//
+	case GL_FILL:
+		iOutline = false;
+		glutSetWindowTitle("Stozek - GL_LINE");
+		RenderScene();
+		break;
+
+		// wyjęcie
+	case EXIT:
+		exit(0);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("Triangles Constructing Fan Example");
+	glutReshapeFunc(ChangeSize);
+	glutSpecialFunc(SpecialKeys);
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	// utworzenie podmenu - Prymitywy
+	int MenuPrimitive = glutCreateMenu(Menu);
+	glutAddMenuEntry("GL_CULL_FACE", GL_CULL_FACE);
+	glutAddMenuEntry("GL_DEPTH_TEST", GL_DEPTH_TEST);
+	glutAddMenuEntry("Polygon mode LINE", GL_LINE);
+	glutAddMenuEntry("Polygon mode FILL", GL_FILL);
+	glutAddMenuEntry("Wyjecie", EXIT);
+
+	// okrelenie przycisku myszki obsługujšcego menu podręczne
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	glutMainLoop();
+	return 0;
+}
---
+++ GK2/GK_zad3/GK_zad3/GK_zad3.cpp
@@ -0,0 +1,350 @@
+﻿// lab2_zad3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdafx.h"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "gltools_extracted.h"
+#include "VectorMath.cpp"
+
+
+#define GL_PI 3.1415f
+// Wielkoci obrotów
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+// Zmiana przestrzeni widocznej i okna.
+// Wywo³ywana w momencie zmiany rozmiaru okna
+void ChangeSize(int w, int h)
+{
+	GLfloat fAspect;
+	// Zabezpieczenie przed dzieleniem przez zero
+	if (h == 0)
+		h = 1;
+	// Zrównanie wielkości widoku i okna
+	glViewport(0, 0, w, h);
+	fAspect = (GLfloat)w / (GLfloat)h;
+	// Ustalenie układu współrzędnych
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Utworzenie rzutowania perspektywicznego
+	gluPerspective(35.0f, fAspect, 1.0, 40.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+// Ta funkcja wykonuje wszystkie konieczne inicjalizacje kontekstu renderowania.
+// Tutaj, konfiguruje i inicjalizuje oświetlenie sceny
+void SetupRC()
+{
+	GLuint texture;
+	int x, y;
+	char pixels[256 * 256];
+	GLint iWidth = 256;
+	GLint iHeight = 256;
+	GLint iComponents = GL_RGB;
+	GLenum eFormat = {
+		GL_RGBA
+	};
+	// Wartoci i współrzdne światła
+	GLfloat whiteLight[] = { 0.05f, 0.05f, 0.05f, 1.0f };
+	GLfloat sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+	GLfloat lightPos[] = { -10.f, 5.0f, 5.0f, 1.0f };
+	glEnable(GL_DEPTH_TEST); // Usuwanie ukrytych powierzchni
+	glEnable(GL_CULL_FACE); // Nie bdziemy prowadzi oblicze wntrza samolotu
+	glFrontFace(GL_CCW); // Wielokty z nawiniciem przeciwnym do ruchu wskazówek zegara
+						 // Włczenie owietlenia
+	glEnable(GL_LIGHTING);
+	// Konfiguracja i włączenie światła numer 0
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, sourceLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	glEnable(GL_LIGHT0);
+	// Włczenie ledzenia kolorów
+	glEnable(GL_COLOR_MATERIAL);
+	// Właciwoci owietlenia otoczenia i rozproszenia
+	// bd ledzi wartoci podawane funkcji glColor
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	// Czarne tło
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Ładowanie tekstury
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Generate a name for the texture
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	for (y = 0; y < 256; y++)
+	{
+		for (x = 0; x < 256; x++)
+			pixels[y * 256 + x] = rand() % 256;
+	}
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, 256, 256, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glEnable(GL_TEXTURE_2D);
+}
+
+// Reakcje na klawisze strzałek
+void SpecialKeys(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+		xRot -= 5.0f;
+	if (key == GLUT_KEY_DOWN)
+		xRot += 5.0f;
+	if (key == GLUT_KEY_LEFT)
+		yRot -= 5.0f;
+	if (key == GLUT_KEY_RIGHT)
+		yRot += 5.0f;
+	xRot = (GLfloat)((const int)xRot % 360);
+	yRot = (GLfloat)((const int)yRot % 360);
+	// Odwieenie zawartoci okna
+	glutPostRedisplay();
+}
+// Wywoływana w celu przerysowania sceny
+void RenderScene(void)
+{
+	GLTVector3 vNormal;
+	GLTVector3 vCorners[13] = {
+		{ 0.0f, 0.80f, 0.0f }, // 0 Góra 
+		{ -0.5f, 0.0f, 0.0f }, // 1 tył środek
+		{ -0.4f, 0.0f, -0.2f }, // 2 tył tył prawy
+		{ -0.2f, 0.0f, -0.4f }, // 3 tył przód prawy
+		{ 0.0f, 0.0f, -0.5f }, // 4 prawy środek
+		{ 0.2f, 0.0f, -0.4f } , // 5 przód tył prawy
+		{ 0.4f, 0.0f, -0.2f } , // 6 przód przód prawy
+		{ 0.5f, 0.0f, 0.0f } , // 7 przód środek
+		{ 0.4f, 0.0f, 0.2f } , // 8 przód przód lewy
+		{ 0.2f, 0.0f, 0.4f } , // 9 przód tył lewy
+		{ 0.0f, 0.0f, 0.5f } , // 10 lewy środek
+		{ -0.2f, 0.0f, 0.4f } , // 11 tył przód lewy
+		{ -0.4f, 0.0f, 0.2f } }; // 12 tył tył lewy
+
+								 // Czyszczenie okna aktualnym kolorem czyszczącym
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Zapisanie stanu macierzy i wykonanie obrotów
+	glPushMatrix();
+	// Cofnięcie obiektów
+	glTranslatef(0.0f, -0.25f, -4.0f);
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+	// Rysowanie piramidy
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glBegin(GL_TRIANGLES);
+
+	//Podstawa piramidy- 10 trójkątów:
+	glNormal3f(0.0f, -1.0f, 0.0f);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[1]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[7]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[2]);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[1]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[12]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[7]);
+	//-----------------------
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[7]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[12]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[8]);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[7]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[6]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[2]);
+	//-----------------------
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[3]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[2]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[6]);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[11]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[8]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[12]);
+	//-----------------------
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[5]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[3]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[6]);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[9]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[8]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[11]);
+	//-----------------------
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[10]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[9]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[11]);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(vCorners[4]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[3]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(vCorners[5]);
+	//-----------------------
+	//Ściany boczne
+	gltGetNormalVector(vCorners[1], vCorners[2], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[1]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[2]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[2], vCorners[3], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[2]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[3]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[3], vCorners[4], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[3]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[4]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[4], vCorners[5], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[4]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[5]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[5], vCorners[6], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[5]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[6]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[6], vCorners[7], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[6]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[7]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[7], vCorners[8], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[7]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[8]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[8], vCorners[9], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[8]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[9]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[9], vCorners[10], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[9]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[10]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[10], vCorners[111], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[10]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[11]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[11], vCorners[12], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[11]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[12]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+	gltGetNormalVector(vCorners[12], vCorners[1], vCorners[0], vNormal);
+	glNormal3fv(vNormal);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex3fv(vCorners[12]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(vCorners[1]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(vCorners[0]);
+
+
+	glEnd();
+	// Odtworzenie stanu macierzy
+	glPopMatrix();
+	// Zamiana buforów
+	glutSwapBuffers();
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Textured Pyramid");
+	glutReshapeFunc(ChangeSize);
+	glutSpecialFunc(SpecialKeys);
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+	return 0;
+}
